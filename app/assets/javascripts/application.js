9eef49e0d4ad4f1c11c3c957977c4817a7523c5a
dctalbot <dctalbot@umich.edu>
2018-02-27 23:59:44 -0500

    Lint

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,31 +10,29 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui/widgets/autocomplete
-//= require jquery-ui/widgets/datepicker
-//= require jquery-ui/widgets/draggable
-//= require jquery-ui/widgets/droppable
-//= require jquery-ui/widgets/tabs
-//= require jquery-ui/widgets/selectable
-//= require turbolinks
-//= require best_in_place
-//= require_tree .
+// = require jquery
+// = require jquery_ujs
+// = require jquery-ui/widgets/autocomplete
+// = require jquery-ui/widgets/datepicker
+// = require jquery-ui/widgets/draggable
+// = require jquery-ui/widgets/droppable
+// = require jquery-ui/widgets/tabs
+// = require jquery-ui/widgets/selectable
+// = require turbolinks
+// = require best_in_place
+// = require_tree .
 
-$(document).on("ready turbolinks:load", function() {
+$(document).on('ready turbolinks:load', function () {
+  jQuery('.best_in_place').best_in_place()
 
-  jQuery(".best_in_place").best_in_place();
-
-  if ( $('[type="date"]').prop('type') != 'date' ) {
+  if ($('[type="date"]').prop('type') != 'date') {
     $("[type='date']").datepicker({
-      dateFormat: 'yy-mm-dd'
-    });
+      dateFormat: 'yy-mm-dd',
+    })
   }
 
-  $(".chzn-select").chosen({width: '100%'});
-
-});
+  $('.chzn-select').chosen({ width: '100%' })
+})
 
 var TAB_KEY_CODE = 9
 
